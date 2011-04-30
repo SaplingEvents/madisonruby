5c7617ba1892e6c8306e8fbad0171ecc2de789bb
Jim Remsik <dev+bigtiger@hashrocket.com>
2011-04-30 11:51:04 -0400

    Only show wireframes when not production

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ MadisonRuby::Application.routes.draw do
   match 'home' => "pages#home", :as => "home"
   match 'speakers' => "pages#speakers", :as => "speakers"
 
-  match 'ui(/:action)', controller: 'ui'
+  unless Rails.env.production?
+    match 'ui(/:action)', controller: 'ui'
+  end
+
   root to: 'pages#home'
 end
