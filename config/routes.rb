179bce404d0622662f9461e92911b81d29160969
Jim Remsik <dev+bigtiger@hashrocket.com>
2011-05-25 16:57:57 -0500

    Add hotel details

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ MadisonRuby::Application.routes.draw do
   match 'speakers' => "pages#speakers", :as => "speakers"
   match 'sponsors' => "pages#sponsors", :as => "sponsors"
   match 'travel' => "pages#travel", :as => "travel"
+  match 'hotels/doubletree' => 'pages#doubletree', :as => "doubletree"
+  match 'hotels/hyatt_place' => 'pages#hyatt_place', :as => "hyatt_place"
+  match 'hotels/inn_on_the_park' => 'pages#inn_on_the_park', :as => "inn_on_the_park"
+  match 'hotels/sheraton' => 'pages#sheraton', :as => "sheraton"
 
   unless Rails.env.production?
     match 'ui(/:action)', controller: 'ui'
