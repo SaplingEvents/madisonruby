5af36c70147e23cf5edacf252f04ecc2139a82cc
Jim Remsik <jim@bendyworks.com>
2012-02-12 16:27:24 -0500

    add Clyde Stubblefield

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,7 @@ module ApplicationHelper
     ["img_homepage-bryan_liles.jpg", "Bryan Liles", "/speakers#bryan_liles"],
     ["img_homepage-jessie_shternshus.jpg", "Jessie Shternshus", "/speakers#jessie_shternshus"],
     ["img_homepage-steve_klabnik.jpg", "Steve Klabnik", "/speakers#steve_klabnik"],
+    ["img_homepage-clyde_stubblefield.jpg", "Clyde Stubblefield", "/speakers#clyde_stubblefield"],
     ["img_homepage-robert_pitts.jpg", "Robert Pitts", "/speakers#robert_pitts"]]
 
   def random_speakers(limit=10)
